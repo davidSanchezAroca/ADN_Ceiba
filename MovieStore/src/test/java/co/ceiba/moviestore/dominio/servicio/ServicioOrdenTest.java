aaebc2fa12924b187ef413d97429dccdec515dfa
davidSanchezAroca <david.sanchez@ceiba.com.co>
2019-11-28 08:40:28 -0500

    test

--- MovieStore/src/test/java/co/ceiba/moviestore/dominio/servicio/ServicioOrdenTest.java
+++ MovieStore/src/test/java/co/ceiba/moviestore/dominio/servicio/ServicioOrdenTest.java
@@ -1,4 +1,6 @@
 package co.ceiba.moviestore.dominio.servicio;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -6,9 +8,12 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 import co.ceiba.moviestore.MovieStoreApplicationTests;
+import co.ceiba.moviestore.aplicacion.comando.ComandoOrden;
+import co.ceiba.moviestore.aplicacion.comando.ComandoOrdenTestDataBuilder;
 import co.ceiba.moviestore.dominio.excepcion.ExcepcionGenerica;
 import co.ceiba.moviestore.dominio.modelo.Orden;
 import co.ceiba.moviestore.dominio.repositorio.RepositorioOrden;
+import co.ceiba.moviestore.dominio.servicio.orden.ServicioActualizarOrden;
 import co.ceiba.moviestore.dominio.servicio.orden.ServicioCrearOrden;
 import co.ceiba.moviestore.dominio.testdatabuilder.OrdenTestDataBuilder;
 
@@ -22,6 +27,19 @@ public class ServicioOrdenTest {
 		orden.setFechaOrden(new SimpleDateFormat("yyyy-MM-dd").parse("2019-11-25"));
 		MovieStoreApplicationTests.assertThrows(()-> servicioCrearOrden.crear(orden),ExcepcionGenerica.class, "No se permite prestar peliculas");
 	}
+	
+	
+	@Test
+	public void actualizar() throws ParseException {
+		Orden orden= new OrdenTestDataBuilder().build();
+		ComandoOrden comando= new ComandoOrdenTestDataBuilder().build();
+		RepositorioOrden repositorio = Mockito.mock(RepositorioOrden.class);		
+		Mockito.when(repositorio.buscarCliente(Mockito.any())).thenReturn(comando);
+		ServicioActualizarOrden servicio= new ServicioActualizarOrden(repositorio);
+		servicio.actualizar(orden);
+		assertEquals(orden.getNumeroOrden(), comando.getNumeroOrden());
+	}
+	
 	
 	
 }
