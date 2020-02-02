ffe2c3639a52eb588607de689f88542d8792616e
JuanDavidSanchezAroca <juan.david748@gmail.com>
2020-02-01 23:18:20 -0500

    seguridad

---
+++ MovieStore/src/main/java/co/ceiba/moviestore/infraestructura/configuration/SecurityConfiguration.java
@@ -0,0 +1,41 @@
+package co.ceiba.moviestore.infraestructura.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableGlobalMethodSecurity
+@Configuration
+public class SecurityConfiguration  extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.
+		    csrf().disable()
+			.authorizeRequests()
+			.anyRequest()
+			.permitAll();
+			//.and().httpBasic()
+            //.and().sessionManagement().disable();
+			//.fullyAuthenticated()
+			//.and()
+			//.formLogin();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+	      .ldapAuthentication()
+	        .userDnPatterns("uid={0},ou=people")
+	        .groupSearchBase("ou=groups")
+	        .contextSource()
+	          .url("ldap://localhost:8389/dc=springframework,dc=org")
+	          .and()
+	        .passwordCompare()
+	          .passwordEncoder(new BCryptPasswordEncoder())
+	          .passwordAttribute("userPassword");
+	}
+}
